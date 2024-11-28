164955bd74fbbe7f02c04ac058d42e6a44d02178
Taiga <alex_sejdija@hotmail.com>
2024-11-28 23:06:28 +0100

    bug fixing

--- src/database.js
+++ src/database.js
@@ -32,7 +32,7 @@ function getMatchHistory(serverID, entries) {
 function getPlayerStats(serverID, playerID) {
     let tableName = "player_stats_" + serverID
     let sql = "SELECT * FROM " + tableName + " WHERE player_name = ?"
-    return database.prepare(sql).get(playerID)
+    return database.prepare(sql).get(playerID) ?? {current_rating: 500}
 }
 function getTableCount(tableName) {
     let sql = "SELECT * FROM " + tableName
@@ -94,6 +94,7 @@ function addResultToDatabase(matchResult, serverID) {
     let tableName = "matches_" + serverID
     let sql = "INSERT INTO " + tableName + "(id,first_player,second_player,score_first_player,score_second_player,winner) VALUES (?,?,?,?,?,?)"
     database.prepare(sql).run(getTableCount(tableName) + 1, matchResult[1], matchResult[2], scoreFirstPlayer, scoreSecondPlayer, winner)
+    console.log("Hi")
     updateStatsDatabase(firstPlayer, secondPlayer, scoreFirstPlayer, scoreSecondPlayer, serverID)
     return false
 }
