ad864403435a036a36d198089b0085ad3e2e7681
Taiga <alex_sejdija@hotmail.com>
2024-12-01 21:41:28 +0100

    changed prefix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const bot = new Discord.Client({
     ],
 })
 // create prefix database for changeability
-let prefix = "_"
+let prefix = "!"
 let queue = {}
 let statuses = [
     "The One Piece is real!",
