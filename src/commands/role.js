c05e4161fb0113e1fe797341113895ccbdaab39e
Taiga <alex_sejdija@hotmail.com>
2024-12-13 23:19:24 +0100

    bug fixing

--- src/commands/role.js
+++ src/commands/role.js
@@ -7,20 +7,26 @@ module.exports = {
         let role
         switch (args[0]) {
             case "create":
+                let existedAlready = true
                 role = interaction.guild.roles.cache.find(role => role.name === interaction.author.username) || null
                 if (!role) {
+                    existedAlready = false
                     await interaction.guild.roles.create({
                         name: interaction.author.username,
                     })
                     role = interaction.guild.roles.cache.find(role => role.name === interaction.author.username)
                 }
                 interaction.guild.members.cache.get(interaction.author.id).roles.add(role);
+                if (existedAlready) {
+                    interaction.reply("Your personal role exists already.")
+                } else {
+                    interaction.reply("Your personal role was successfully set up!")
+                }
                 break;
             case "change":
                 role = interaction.guild.roles.cache.find(role => role.name === interaction.author.username)
                 try {
                     await role.edit({color: args[1]})
-                    console.log("hi")
                 } catch {
                     interaction.reply("This isn't a valid color code.")
                 }
